db674e6788f5bfdf37a3e7f9751bd85adbcf9cd5
ejaz-ul-haq <54179609+ejaz-ul-haq@users.noreply.github.com>
2024-05-16 04:56:44 +0500

    Ejaz - fix-03

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-set -e
+#set -e
 
 echo "Deployment started ..."
 
 # Enter maintenance mode or return true
 # if already is in maintenance mode
-(php artisan down) || true
+#(php artisan down) || true
 
 # Pull changes from GitHub
 git pull origin main
 
 # Install composer dependencies
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+#composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
 # Clear the old cache and optimize Laravel
 php artisan clear-compiled
@@ -26,6 +26,6 @@ php artisan optimize
 php artisan migrate:refresh --seed --force
 
 # Exit maintenance mode
-php artisan up
+#php artisan up
 
 echo "Deployment finished!"
